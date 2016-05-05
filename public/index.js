97335349d522a82b02d00dd67652b73ea9f01320
David Selby <david.selby@burberry.com>
2016-05-05 23:38:56 +0100

    Nav styling

--- public/index.js
+++ public/index.js
@@ -8,12 +8,12 @@ import Device from '../views/Devices'
 import Run from '../views/Run'
 
 render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="/features" component={Feature} selectable="true" />
-      <Route path="/devices" component={Device} selectable="false" />
-      <Route path="/run" component={Run} selectable="true" />
-    </Route>
-  </Router>
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			<IndexRoute component={Home}/>
+			<Route path="/features" component={Feature} selectable="true" />
+			<Route path="/devices" component={Device} selectable="false" />
+			<Route path="/run" component={Run} selectable="true" />
+		</Route>
+	</Router>
 ), document.getElementById('content'))
--- views/App.js
+++ views/App.js
@@ -5,21 +5,22 @@ import Features from './Features'
 import Devices from './Devices'
 
 export default React.createClass({
-  render() {
-    return (
-      <div className="page">
-        <div className="app-header">
-          <ul className="nav nav-tabs" role="nav">
-            <li><NavLink to="/" onlyActiveOnIndex className="app-title">Arachne</NavLink></li>
-            <li><NavLink to="/features">Features</NavLink></li>
-            <li><NavLink to="/devices">Devices</NavLink></li>
-            <li><NavLink to="/run">Run Tests</NavLink></li>
-          </ul>
-        </div>
-        <div className="container">
-          {this.props.children}
-        </div>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="page">
+				<div className="app-header">
+					<ul className="nav nav-tabs" role="nav">
+						<li className="nav-top nav-home"><NavLink to="/" onlyActiveOnIndex className="app-title"><img className="logo" src={'assets/arachne.png'} width="50px" height="40px"/>Arachne</NavLink></li>
+						<li className="nav-top"><NavLink to="/features">Features</NavLink></li>
+						<li className="nav-top"><NavLink to="/devices">Devices</NavLink></li>
+						<li className="nav-top"><NavLink to="/run">Run Tests</NavLink></li>
+
+					</ul>
+				</div>
+				<div className="container">
+					{this.props.children}
+				</div>
+			</div>
+		)
+	}
 })
